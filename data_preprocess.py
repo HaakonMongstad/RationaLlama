95577188aa5d32f4b3a44e349e2fd58214bec8a4
HaakonMongstad <haakon@utexas.edu>
2024-04-24 17:17:59 -0500

    fixed data_preprocess... sorry for pushing to main

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,5 +1,3 @@
-# read in 'name' and 'steps' columns from RAW_recipes.csv into a pd.DataFrame.
-
 import sys
 
 import pandas as pd
@@ -7,26 +5,43 @@ import pandas as pd
 
 def read_data(file_path):
     data = pd.read_csv(file_path)
-    data = data[["name", "steps"]]
+    data = data[["name", "ingredients", "steps"]]
     return data
 
 
 def preprocess(file_path):
     data = read_data(file_path)
 
-    # each element in the steps column is a list of strings. Please loop through the list and convert the list to one string, with each elemetn from the list numbers 1-(len(list)) followed by a period and a new line.
+    # create query column with name of recipe
+    data["query"] = data["name"]
+
+    # edit ingredients to be one string
+    data["ingredients"] = data["ingredients"].apply(
+        lambda x: "\n".join(
+            [str(i + 1) + ". " + ingredient for i, ingredient in enumerate(eval(x))]
+        )
+    )
+
+    # edit steps to be one string
     data["steps"] = data["steps"].apply(
         lambda x: "\n".join(
             [str(i + 1) + ". " + step for i, step in enumerate(eval(x))]
         )
     )
 
+    # create response column with ingredients and steps
+    data["response"] = data.apply(
+        lambda row: f"You will need these ingredients: \n{row['ingredients']} \nHere are the steps to make the recipe: \n{row['steps']}\n",
+        axis=1,
+    )
+
+    # drop name, ingredients, and steps columns
+    data = data[["query", "response"]]
+
     return data
 
 
 if __name__ == "__main__":
-    # use command line prompt to run this script given source and output file paths
-
     if len(sys.argv) != 3:
         print("Usage: python data_preprocess.py <source_file_path> <output_file_path>")
         sys.exit(1)
